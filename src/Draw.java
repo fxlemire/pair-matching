d459a3e025f778ef8ac3c70880938feab91df972
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2015-11-19 17:01:21 -0500

    now sends from outlook // session has authenticator // transport method changed from smtps to smtp

--- src/Draw.java
+++ src/Draw.java
@@ -10,8 +10,9 @@ public class Draw {
 	private final static Member EDWARD = new Member("Edward Carnby", "edward@infogrames.com");
 	
 	private final static String SENDER = "gordon@hl.com";
-	private final static String HOST = "smtp.gmail.com";
-	private final static String USERNAME = "gordon";
+	private final static String HOST = "smtp-mail.outlook.com";
+	private final static String PORT = "587";
+	private final static String USERNAME = SENDER;
 	private final static String PASSWORD = "halflife3";
 	
 	public static void main(String[] args) {
@@ -32,6 +33,7 @@ public class Draw {
 	private static ArrayList<Member> getMembers() {
 		ArrayList<Member> members = new ArrayList<Member>();
 		members.add(GORDON);
+		members.add(EDWARD);
 		return members;
 	}
 	
@@ -61,25 +63,32 @@ public class Draw {
 	 * @return
 	 */
 	private static boolean isValidPair(Member giver, Member receiver) {
+		boolean isValid = true;
+
 		if (giver.getEmail().equals(receiver.getEmail())) {
-			return false;
+			isValid = false;
 		}
-		
+
 		/* add extra conditions here */
 		
-		return true;
+		return isValid;
 	}
 	
 	private static void sendEmail(ArrayList<Member> members, HashMap<Member, Member> matches) {
 		System.out.println("Setting all the connection settings...");
 	    Properties properties = getMailProperties();
-	    Session session = Session.getInstance(properties);
+		Authenticator authenticator = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(USERNAME, PASSWORD);
+			}
+		};
+	    Session session = Session.getInstance(properties, authenticator);
 	    
 		for (int i = 0; i < matches.size(); i++) {
-		    String to = members.get(i).getEmail();
-		    String giftReceiver = matches.get(members.get(i)).getName();
+		    Member emailRecipient = members.get(i);
+		    String giftReceiver = matches.get(emailRecipient).getName();
 		    
-			send(SENDER, to, HOST, USERNAME, PASSWORD, session, giftReceiver);
+			send(SENDER, emailRecipient, HOST, USERNAME, PASSWORD, session, giftReceiver);
 			
 			System.out.println("Sent message successfully... (" + (i + 1) + "/" + members.size() + ")");
 		}
@@ -87,13 +96,20 @@ public class Draw {
 	
 	private static Properties getMailProperties() {
 		Properties properties = new Properties();
-	    properties.setProperty("mail.smtp.host", HOST);
-	    properties.setProperty("mail.user", USERNAME);
-	    properties.setProperty("mail.password", PASSWORD);
+		properties.put("mail.smtp.host", HOST);
+		properties.put("mail.smtp.port", PORT);
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.user", USERNAME);
+		properties.put("mail.password", PASSWORD);
+		properties.put("mail.smtp.socketFactory.port", PORT);
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		properties.put("mail.smtp.socketFactory.fallback", "false");
+
 	    return properties;
 	}
 
-	private static void send(String from, String to, String host, String username, String password, Session session, String giftReceiver) {
+	private static void send(String from, Member emailRecipient, String host, String username, String password, Session session, String giftReceiver) {
 		try {
 		    // Create a default MimeMessage object.
 		    MimeMessage message = new MimeMessage(session);
@@ -102,20 +118,20 @@ public class Draw {
 		    message.setFrom(new InternetAddress(from));
 
 		    // Set To: header field of the header.
-		    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		    message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailRecipient.getEmail()));
 
 		    // Set Subject: header field
-		    message.setSubject("Your draw result!");
+			message.setSubject("Your draw result!");
 
 		    // Send the actual HTML message, as big as you like
 		    message.setContent(
-		    		"<body>" + 
-		        		"<h1>Your draw: " + giftReceiver + "</h1>" +
-			        "</body>"
-		    		, "text/html");
+					"<body>" +
+						"<h1>Your draw: " + giftReceiver + "</h1>" +
+						"</body>"
+					, "text/html");
 
 		    // Send message
-		    Transport t = session.getTransport("smtps");
+		    Transport t = session.getTransport("smtp");
 		    t.connect(host, username, password);
 		    t.sendMessage(message, message.getAllRecipients());
 		    t.close();
