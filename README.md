694d60464e926a45d0095fc3385a35adbaf31d2b
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2017-10-30 12:37:34 -0400

    removed all hardcoded values // all config taken from config.json // get user confirmation before sending emails

--- README.md
+++ README.md
@@ -6,10 +6,73 @@ A good use would be for Secret Santa.
 
 Enter the people you need to match together, and this program will send an e-mail to all participants telling them who is their pick.
 
-There is no interface yet, you need to hardcode your e-mail, people and matching conditions manually.
+# How To Use
+
+Create a file `config.json` in your root directory with the following keys and without comments:
+
+```
+"SEND": Boolean // true will send the emails, false will print results to the console
+"email": {
+  // html string starting with the <body> tag. Use the variable giftReceiver to display the picked person
+  "content": String
+  "override": { // optional object that allows to override some email configurations
+    [email: String]: { // email of the recipient taken from members array
+      "subject": String // subject of the email to override
+      "to": String // overrides the name that will be used, e.g. "name" <member@email.com>
+    }
+  },
+  "subject": String // use the variable emailRecipient to display the name of the recipient
+},
+"members": [
+  {
+    "email": String,
+    "name": String
+  }
+],
+"rules": { // optional rules that will reject pairings
+  "couples": [ // forbid member1 to pick member2 and vice-versa
+    [member1.email, member2.email] // use the member emails
+  ],
+  "pastyears": [ // forbid member1 to pick member2
+    [member1.email, member2.email] // use the member emails
+  ]
+},
+"sender": {
+  "from": String // as you want it to appear for the receivers,
+  "host": String // smtp server,
+  "port": String // smtp port,
+  "username": String // sender email,
+  "password": String // sender password
+}
+```
+
+See [config.example.json](config.example.json);
+
+Sender config example for Gmail:
+```
+{
+  "from": "Santa Claus <santa@gmail.com>",
+  "host": "smtp.gmail.com",
+  "port": "587",
+  "username": "santa@gmail.com",
+  "password": "SantasMagicPassword"
+}
+```
+
+Sender config example for Outlook:
+```
+{
+  "from": "Santa Claus <santa@outlook.com>",
+  "host": "smtp-mail.outlook.com",
+  "port": "587",
+  "username": "santa@outlook.com",
+  "password": "SantasMagicPassword"
+}
+```
 
 # Compile and run
 
-`javac -cp ".;javax.mail.jar" -d target src/Draw.java src/Member.java`
-
-`java -cp "target;javax.mail.jar" Draw`
+```
+javac -cp ".;libs/*" -d target src/*
+java -cp "target;libs/*" Draw
+```
--- src/Draw.java
+++ src/Draw.java
@@ -1,44 +1,81 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;
-
+import java.util.Scanner;
 import javax.mail.*;
 import javax.mail.internet.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Draw {
-	private final static Member GORDON = new Member("Gordon Freeman", "gordon@hl.com");
-	private final static Member EDWARD = new Member("Edward Carnby", "edward@infogrames.com");
+	private static JSONObject CONFIG;
+	private static String FROM;
+	private static String HOST;
+	private static String PORT;
+	private static String USERNAME;
+	private static String PASSWORD;
 
-	private final static String SENDER = "Gordon Freeman <gordon@hl.com>";
-	private final static String HOST = "smtp-mail.outlook.com";
-	private final static String PORT = "587";
-	private final static String USERNAME = "gordon@hl.com";
-	private final static String PASSWORD = "halflife3";
+	public static void main(String[] args) throws FileNotFoundException, JSONException {
+		CONFIG = new JSONObject(new Scanner(new File("config.json")).useDelimiter("\\Z").next());
 
-	public static void main(String[] args) {
+		setSenderAttributes();
+
 		ArrayList<Member> members = getMembers();
-
 		HashMap<Member, Member> matches = getGiftMatches(members);
 
 		System.out.println("All the matches are now done...");
-		System.out.println("About to send emails to all recipients...");
+		if (CONFIG.getBoolean("SEND")) {
+			Scanner kbd = new Scanner(System.in);
 
-		// sendEmail(members, matches); // Uncomment sendEmail when ready to send your matches
-		printMatches(members, matches);
+			System.out.print("Emails will be sent. Do you wish to continue? (Y/n) ");
+
+			String decision = kbd.nextLine();
+
+			if (decision.toLowerCase().equals("y") || decision.equals("")) {
+				sendEmail(members, matches);
+			} else {
+				System.out.println("Operation aborted.");
+			}
+		} else {
+			printMatches(members, matches);
+		}
+	}
+
+	/**
+	 * Sets all private sender attributes
+	 */
+	private static void setSenderAttributes() throws JSONException {
+		JSONObject sender = CONFIG.getJSONObject("sender");
+
+		FROM = sender.getString("from");
+		HOST = sender.getString("host");
+		PORT = sender.getString("port");
+		USERNAME = sender.getString("username");
+		PASSWORD = sender.getString("password");
 	}
 
 	/**
 	 * Add all the final static Member
 	 * @return ArrayList<Member> of all the final static Member
 	 */
-	private static ArrayList<Member> getMembers() {
+	private static ArrayList<Member> getMembers() throws JSONException {
 		ArrayList<Member> members = new ArrayList<Member>();
-		members.add(GORDON);
-		members.add(EDWARD);
+		JSONArray parsedMembers = CONFIG.getJSONArray("members");
+
+		for (int i = 0; i < parsedMembers.length(); ++i) {
+			String email = parsedMembers.getJSONObject(i).getString("email");
+			String name = parsedMembers.getJSONObject(i).getString("name");
+			Member m = new Member(name, email);
+			members.add(m);
+		}
+
 		return members;
 	}
 
-	private static HashMap<Member, Member> getGiftMatches(ArrayList<Member> members) {
+	private static HashMap<Member, Member> getGiftMatches(ArrayList<Member> members) throws JSONException {
 		ArrayList<Member> receivers = getMembers();
 		HashMap<Member, Member> matches = new HashMap<Member, Member>();
 
@@ -76,19 +113,51 @@ public class Draw {
 	 * @param receiver
 	 * @return
 	 */
-	private static boolean isValidPair(Member giver, Member receiver) {
-		boolean isValid = true;
-
+	private static boolean isValidPair(Member giver, Member receiver) throws JSONException {
 		if (giver.getEmail().equals(receiver.getEmail())) {
-			isValid = false;
+			return false;
 		}
 
-		/* add extra conditions here */
+		JSONObject rules = CONFIG.isNull("rules") ? null : CONFIG.getJSONObject("rules");
 
-		return isValid;
+		if (rules == null) {
+			return true;
+		}
+
+		JSONArray couples = rules.isNull("couples") ? null : rules.getJSONArray("couples");
+
+		if (couples != null) {
+			for (int i = 0; i < couples.length(); ++i) {
+				JSONArray rule = couples.getJSONArray(i);
+				String forbid1 = rule.getString(0);
+				String forbid2 = rule.getString(1);
+
+				if ((giver.getEmail().equals(forbid1) && receiver.getEmail().equals(forbid2)) ||
+				(giver.getEmail().equals(forbid2) && receiver.getEmail().equals(forbid1))) {
+					return false;
+				}
+			}
+		}
+
+		JSONArray pastyears = rules.isNull("pastyears") ? null : rules.getJSONArray("pastyears");
+
+		if (couples != null) {
+			for (int i = 0; i < pastyears.length(); ++i) {
+				JSONArray rule = pastyears.getJSONArray(i);
+				String forbid1 = rule.getString(0);
+				String forbid2 = rule.getString(1);
+
+				if (giver.getEmail().equals(forbid1) && receiver.getEmail().equals(forbid2)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 
-	private static void sendEmail(ArrayList<Member> members, HashMap<Member, Member> matches) {
+	private static void sendEmail(ArrayList<Member> members, HashMap<Member, Member> matches) throws JSONException {
+		System.out.println("About to send emails to all recipients...");
 		System.out.println("Setting all the connection settings...");
 		Properties properties = getMailProperties();
 
@@ -104,7 +173,7 @@ public class Draw {
 			Member emailRecipient = members.get(i);
 			String giftReceiver = matches.get(emailRecipient).getName();
 
-			send(SENDER, emailRecipient, HOST, USERNAME, PASSWORD, session, giftReceiver);
+			send(emailRecipient, session, giftReceiver);
 
 			System.out.println("Sent message successfully... (" + (i + 1) + "/" + members.size() + ")");
 		}
@@ -125,34 +194,36 @@ public class Draw {
 		return properties;
 	}
 
-	private static void send(String from, Member emailRecipient, String host, String username, String password, Session session, String giftReceiver) {
+	private static void send(Member emailRecipient, Session session, String giftReceiver) throws JSONException {
 		try {
+			JSONObject emailConfig = CONFIG.getJSONObject("email");
+			JSONObject override = emailConfig.isNull("override") ? null : emailConfig.getJSONObject("override");
+			JSONObject memberOverride = override != null && !override.isNull(emailRecipient.getEmail()) ? override.getJSONObject(emailRecipient.getEmail()) : null;
+
 			// Create a default MimeMessage object.
 			MimeMessage message = new MimeMessage(session);
 
 			// Set From: header field of the header.
-			message.setFrom(new InternetAddress(from));
+			message.setFrom(new InternetAddress(FROM));
 
 			// Set To: header field of the header.
-			String recipient = emailRecipient.getName() + " <" + emailRecipient.getEmail() + ">";
-
+			String recipientName = memberOverride != null && !memberOverride.isNull("to") ? memberOverride.getString("to") : emailRecipient.getName();
+			String recipient = recipientName + " <" + emailRecipient.getEmail() + ">";
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 
 			// Set Subject: header field
-			String subject = emailRecipient.getName() + "! Here is your draw result!";
-
+			String subject = memberOverride != null && !memberOverride.isNull("subject") ? memberOverride.getString("subject") : emailConfig.getString("subject");
+			subject = subject.replaceAll("emailRecipient", emailRecipient.getName());
 			message.setSubject(subject);
 
-			// Send the actual HTML message, as big as you like
-			message.setContent(
-				"<body>" +
-					"<h1>Your draw: " + giftReceiver + "</h1>" +
-				"</body>"
-			, "text/html");
+			// Send the actual HTML message
+			String content = emailConfig.getString("content").replaceAll("giftReceiver", giftReceiver);
+
+			message.setContent(content, "text/html");
 
 			// Send message
 			Transport t = session.getTransport("smtp");
-			t.connect(host, username, password);
+			t.connect(HOST, USERNAME, PASSWORD);
 			t.sendMessage(message, message.getAllRecipients());
 			t.close();
 		} catch (MessagingException mex) {
